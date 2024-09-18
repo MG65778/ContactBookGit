2f289f0820e4431dc6b9f31a95fd23dcaa475bfe
Miguel <m.gramacho@campus.fct.unl.pt>
2024-09-18 15:51:42 +0100

    Updated the code to pass test 2 Added the command GN (Get Name) using the new method getName in Main class. In the class ContactBook I added the method hasContact, getName and searchIndex so that the new command could be added.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -20,6 +20,10 @@ public class ContactBook {
         return searchIndex(name) >= 0;
     }
 
+    public boolean hasContact(int phone) {
+        return searchIndex(phone) >= 0;
+    }
+
     public int getNumberOfContacts() {
         return counter;
     }
@@ -51,6 +55,11 @@ public class ContactBook {
     }
 
     //Pre: name != null && hasContact(name)
+    public String getName(int phone) {
+        return contacts[searchIndex(phone)].getName();
+    }
+
+    //Pre: name != null && hasContact(name)
     public void setPhone(String name, int phone) {
         contacts[searchIndex(name)].setPhone(phone);
     }
@@ -66,6 +75,19 @@ public class ContactBook {
         boolean found = false;
         while (i<counter && !found)
             if (contacts[i].getName().equals(name))
+                found = true;
+            else
+                i++;
+        if (found) result = i;
+        return result;
+    }
+
+    private int searchIndex(int phone) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i<counter && !found)
+            if (contacts[i].getPhone()==phone)
                 found = true;
             else
                 i++;
